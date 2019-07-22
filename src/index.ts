93a5fd50400a0968d651fc5d0504bc9bb5a403c6
Johan Stén <johan@futuretense.io>
2019-07-22 17:45:39 +0800

    Export classes

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ export { Network } from './network';
 export { Keypair } from './keypair';
 export { Client, client } from './client';
 export { Faucet, faucet } from './faucet';
+export { Transaction, SignedTransaction } from './transaction';
 export { storage } from './storage';
 export * from './utils';
