1aed1cd7d75a584e3da6ed99bcc351e7b974f052
Johan Stén <johan@futuretense.io>
2019-07-26 01:02:16 +0800

    Extract `some`

--- src/cluster.ts
+++ src/cluster.ts
@@ -82,38 +82,3 @@ function getConsensusMessageFromResponse(
     const index = hashes.indexOf(modeKey);
     return responses[index];
 }
-
-/**
- * A promise wrapper that either resolves when `threshold` or more of the
- * supplied promises are resolved, and rejects otherwise.
- *
- * @param promises
- * @param threshold
- */
-
-function some(
-    promises: Promise<any>[],
-    threshold: number
-): Promise<any> {
-
-    const failure = promises.length - threshold + 1;
-    let numFailures = 0;
-    let numSuccesses = 0;
-
-    return new Promise((resolve, reject) => {
-        promises.map(p => p
-            .then(res => {
-                numSuccesses += 1;
-                if (numSuccesses === threshold) {
-                    resolve(res);
-                }
-            })
-            .catch(err => {
-                numFailures += 1;
-                if (numFailures === failure) {
-                    reject(err);
-                }
-            })
-        );
-    });
-}
---
+++ src/promise-some.ts
@@ -0,0 +1,36 @@
+
+/**
+ * A promise wrapper that either resolves when `threshold` or more of the
+ * supplied promises are resolved, and rejects otherwise.
+ *
+ * @hidden
+ * @param promises
+ * @param threshold
+ */
+
+export function some(
+    promises: Promise<any>[],
+    threshold: number
+): Promise<any> {
+
+    const failure = promises.length - threshold + 1;
+    let numFailures = 0;
+    let numSuccesses = 0;
+
+    return new Promise((resolve, reject) => {
+        promises.map(p => p
+            .then(res => {
+                numSuccesses += 1;
+                if (numSuccesses === threshold) {
+                    resolve(res);
+                }
+            })
+            .catch(err => {
+                numFailures += 1;
+                if (numFailures === failure) {
+                    reject(err);
+                }
+            })
+        );
+    });
+}
